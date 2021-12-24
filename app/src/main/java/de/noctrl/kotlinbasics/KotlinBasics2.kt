24c822441d2c1257daa15314a29d57edb9310023
no-CTRL91 <no.ctrl19@gmail.com>
2021-12-24 17:37:04 +0100

    more tutorial stuff (bis Kapitel 4)

--- app/src/main/java/de/noctrl/kotlinbasics/KotlinBasics2.kt
+++ app/src/main/java/de/noctrl/kotlinbasics/KotlinBasics2.kt
@@ -1,12 +1,30 @@
 package de.noctrl.kotlinbasics
 
 fun main() {
+
+    //NULLABLES
+    println("NULLABLES------------------------------------------------------------------------------")
     var name: String = "Talha"
-    //name = null      compiler Error
-    var nullName: String? = "Talha"
+    //name = null   |   compiler Error
+    var nullName: String? = null
 
     var lange = name.length
-    //var lange2 = nullName.length   da nullName null sein könnte gibt es compiler error
+    //var lange2 = nullName.length  |    da nullName null sein könnte gibt es compiler error
     var lange2 = nullName?.length
-    print("nib")
+    // das ? ist eine abfrage ob !null. Wenn true dann wird (hier) .length ausgeführt
+    println(nullName?.length)
+    nullName?.let { println(it.length) }  //keine Ahnung was .let macht -> Lambda glaub ich
+
+    // ?: ELVIS OPERATOR wandelt nullable String (also String?) in normalen
+    var newName = nullName ?: "gast"
+    println("Hallo $newName")
+
+    // !! NOT-NULL-ASSERTION
+    var nullAge: Int? = 32
+    var age: Int = nullAge!!
+    nullAge!!.compareTo(4)
+    println(age)
+
+
+
 }

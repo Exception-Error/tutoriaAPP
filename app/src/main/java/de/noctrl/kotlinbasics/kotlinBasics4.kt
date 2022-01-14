2d0bd5857978afaae609e64a2292b8e51bd3fe52
no-CTRL91 <no.ctrl19@gmail.com>
2022-01-14 16:46:39 +0100

    switched to viewBinding added kotlionBasics4

---
+++ app/src/main/java/de/noctrl/kotlinbasics/kotlinBasics4.kt
@@ -0,0 +1,10 @@
+package de.noctrl.kotlinbasics
+
+fun main(){
+    myFunction(5)
+}
+
+fun myFunction(a:Int){
+    var a = 10
+    println("a ist $a")
+}
--- app/src/main/java/de/noctrl/tutoriaapp/MainActivity.kt
+++ app/src/main/java/de/noctrl/tutoriaapp/MainActivity.kt
@@ -2,32 +2,28 @@ package de.noctrl.tutoriaapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
+import de.noctrl.tutoriaapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        //diese Variablen deklarationen werden nur gebraucht wenn import kotlinx.android.synthetic.main.activity_main.* nicht benutzt wird
-        /*
-        val counterTxt = findViewById<TextView>(R.id.TextView1)
-        val countButton = findViewById<Button>(R.id.button1)
-        val resetButton = findViewById<Button>(R.id.button2)
-        */
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         var counter = 0
 
-        counterTxt.text = "$counter"
-        
-        countButton.setOnClickListener { 
+        binding.counterTxt.text = "$counter"
+
+        binding.countButton.setOnClickListener {
             counter++
-            counterTxt.text = "$counter"
+            binding.counterTxt.text = "$counter"
         }
-        
-        resetButton.setOnClickListener{
-            counter=0
-            counterTxt.text = "$counter"
+
+        binding.resetButton.setOnClickListener {
+            counter = 0
+            binding.counterTxt.text = "$counter"
         }
+
     }
 }
